53c3d62f3c2c318634cd5ba016e81e9aac9f23d5
Mikhail Pirozhkov <mipiyu@gmail.com>
2016-02-09 12:09:39 +0300

    create BoysAndGirls

---
+++ BoysAndGirls.php
@@ -0,0 +1,53 @@
+<?php
+include 'BGclass.php';
+//boys 5 person
+class Stan extends Boy
+{
+	
+}
+
+class Kyle extends Boy
+{
+	
+}
+
+class Kenny extends Boy
+{
+	
+}
+
+class Eric extends Boy
+{
+	
+}
+
+class Leopold extends Boy
+{
+	
+}
+
+//girls 5 person
+class Leslie extends Girl
+{
+	
+}
+
+class Wendy extends Girl
+{
+	
+}
+
+class Lola extends Girl
+{
+
+}
+
+class Heidi extends Girl
+{
+
+}
+
+class Jessie extends Girl
+{
+
+}
